bb024057a281b9e8dc35c76869ad179dd69e7b2a
Alexey Krasilnikov <alexey.a.krasilnikov@gmail.com>
2012-03-05 18:44:18 +0800

    styling

--- src/org/example/cryptopass/StartActivity.java
+++ src/org/example/cryptopass/StartActivity.java
@@ -9,27 +9,23 @@ import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 
-public class StartActivity extends ListActivity implements OnItemClickListener
-{
-	private static class BookmarksAdapter extends ResourceCursorAdapter
-	{
-		public BookmarksAdapter(Context context, Cursor c)
-		{
-			super(context, R.layout.bookmark, c, true);
+public class StartActivity extends ListActivity implements OnItemClickListener {
+	private static final int INVALID_ID = -1;
+
+	private class BookmarksAdapter extends ResourceCursorAdapter {
+		public BookmarksAdapter(Cursor c) {
+			super(StartActivity.this, R.layout.bookmark, c, true);
 		}
 
 		@Override
-		public void bindView(View view, Context context, Cursor cursor)
-		{
+		public void bindView(View view, Context context, Cursor cursor) {
 			TextView tv = (TextView) view;
 
 			String url = cursor.getString(BookmarksHelper.URL_COLUMN);
@@ -44,10 +40,11 @@ public class StartActivity extends ListActivity implements OnItemClickListener
 	private BookmarksHelper helper;
 	private Cursor bookmarksCursor;
 
-	/** Called when the activity is first created. */
+	/**
+	 * Called when the activity is first created.
+	 */
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		helper = new BookmarksHelper(this);
@@ -55,62 +52,51 @@ public class StartActivity extends ListActivity implements OnItemClickListener
 		bookmarksCursor = helper.queryBookmarks();
 		startManagingCursor(bookmarksCursor);
 
-		if (bookmarksCursor.getCount() == 0)
-		{
+		if (bookmarksCursor.getCount() == 0) {
 			startActivity(new Intent(this, MainActivity.class));
 			finish();
-		}
-		else
-		{
-			setContentView(R.layout.start);
-
+		} else {
 			ListView listView = getListView();
 
+			TextView headerView = (TextView) getLayoutInflater().inflate(R.layout.bookmark, listView, false);
+			headerView.setText("Empty");
+
+			listView.addHeaderView(headerView);
 			listView.setOnItemClickListener(this);
 			registerForContextMenu(listView);
 
-			setListAdapter(new BookmarksAdapter(this, bookmarksCursor));
-
-			Button newButton = (Button) findViewById(R.id.newButton);
-			newButton.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View arg0)
-				{
-					startMainEmpty();
-				}
-			});
+			setListAdapter(new BookmarksAdapter(bookmarksCursor));
 		}
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-	{
-		startMainBookmark(id);
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		if (INVALID_ID != id) {
+			startMainBookmark(id);
+		} else {
+			startMainEmpty();
+		}
 	}
 
-	void startMainEmpty()
-	{
+	void startMainEmpty() {
 		startActivity(new Intent(StartActivity.this, MainActivity.class));
 	}
 
-	void startMainBookmark(long id)
-	{
+	void startMainBookmark(long id) {
 		Bookmark bookmark = helper.getBookmark(id);
-		
+
 		Intent intent = new Intent(this, MainActivity.class);
 		intent.putExtra(MainActivity.EXTRA_URL, bookmark.url);
 		intent.putExtra(MainActivity.EXTRA_USERNAME, bookmark.username);
 
 		startActivity(intent);
 	}
-	
+
 	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
-	{
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
 
-		if (v == getListView())
-		{
+		if (v == getListView()) {
 			AdapterContextMenuInfo adapterMenuInfo = (AdapterContextMenuInfo) menuInfo;
 
 			getMenuInflater().inflate(R.menu.context, menu);
@@ -120,26 +106,26 @@ public class StartActivity extends ListActivity implements OnItemClickListener
 			menu.setHeaderTitle(tv.getText());
 		}
 	}
-	
+
 
 	@Override
-	public boolean onContextItemSelected(MenuItem item)
-	{
+	public boolean onContextItemSelected(MenuItem item) {
 		AdapterContextMenuInfo menuInfo = (AdapterContextMenuInfo) item.getMenuInfo();
 
-		switch (item.getItemId())
-		{
-		case R.id.open:
-			startMainBookmark(menuInfo.id);
+		if (INVALID_ID != menuInfo.id) {
+			switch (item.getItemId()) {
+				case R.id.open:
+					startMainBookmark(menuInfo.id);
 
-			return true;
-			
-		case R.id.delete:
-			helper.deleteBookmark(menuInfo.id);
-			
-			bookmarksCursor.requery();
-			
-			return true;
+					return true;
+
+				case R.id.delete:
+					helper.deleteBookmark(menuInfo.id);
+
+					bookmarksCursor.requery();
+
+					return true;
+			}
 		}
 
 		return super.onContextItemSelected(item);
